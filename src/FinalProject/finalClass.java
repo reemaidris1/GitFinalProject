326e59ec270519344823ba69cf376634379fb05f
reemaidris1 <reema.idris0@gmail.com>
2023-01-02 01:58:21 -0500

    first commit

---
+++ src/FinalProject/finalClass.java
@@ -0,0 +1,12 @@
+package FinalProject;
+
+public class finalClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("this is the code for the mercedes");
+		
+	}
+
+}
